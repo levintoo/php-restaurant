52dc5ce9a3095c4e6bbf5cf2dfb390b3392d2473
levintoo <tooklevin@gmail.com>
2021-08-09 23:44:02 +0300

    partials

---
+++ admin/partials/menu.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/admin.css">
+
+    <title>Food Order website Admin</title>
+</head>
+
+<body>
+    <!-- menu section start  -->
+    <section class="menu">
+        <div class="wrapper">
+            <!-- navbar section -->
+            <nav class="navbar navbar-expand-lg navbar-light m-lg-0 p-lg-0">
+                <div class="container-fluid">
+                    <a class="navbar-item" href="#"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse menu text-right " id="navbarText">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 col-12 justify-content-center">
+                            <li class="nav-item">
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="categories.html">Admin</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="foods.html">category</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#">Food</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#">Order</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar Section Starts Here -->
+
+        </div>
+    </section>
+    <!-- menu section end  -->
