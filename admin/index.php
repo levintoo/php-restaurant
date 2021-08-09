97144d673ebe8d65239f3ccb936c039604fb7f67
levintoo <tooklevin@gmail.com>
2021-08-09 19:30:52 +0300

    admin page

--- admin/index.php
+++ admin/index.php
@@ -4,10 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/admin.css">
     <title>Food Order website Admin</title>
 </head>
 <body>
-    
-    
+    <!-- menu section start  -->
+    <section class="menu">
+        manu goes here
+    </section>
+    <!-- menu section end  -->
+
+    <!-- main content section start  -->
+    <section class="main-content">
+        main content goes here
+    </section>
+    <!-- main content section end  -->
+
+    <!-- footer section start  -->
+    <section class="footer">
+        footer goes here
+    </section>
+    <!-- footer section end  -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 </html>
