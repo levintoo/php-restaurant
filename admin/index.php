a805d6bf38d5b70ff1a72be22a92173f8ae2d951
levintoo <tooklevin@gmail.com>
2021-08-09 20:03:15 +0300

    admin panel navbar

--- admin/index.php
+++ admin/index.php
@@ -6,24 +6,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Food Order website Admin</title>
 </head>
 <body>
     <!-- menu section start  -->
     <section class="menu">
-        manu goes here
+        <div class="wrapper">
+        <!-- navbar section -->
+ <nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container-fluid">
+        <a class="navbar-item" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse menu text-right" id="navbarText">
+            <!-- <div class="navbar-nav me-auto mb-2 mb-lg-0 "></div> -->
+            <div class="navbar-nav me-auto mb-2 mb-lg-0 "></div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                <li class="nav-item">
+                    <a href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="categories.html">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a href="foods.html">category</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">Food</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">Order</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+<!-- Navbar Section Starts Here -->
+
+        </div>
     </section>
     <!-- menu section end  -->
 
     <!-- main content section start  -->
     <section class="main-content">
-        main content goes here
+    <div class="wrapper">
+        manu goes here
+            
+        </div>
     </section>
     <!-- main content section end  -->
 
     <!-- footer section start  -->
+
     <section class="footer">
-        footer goes here
+        <div class="container wrapper text-center">
+            <p>All rights reserved. Designed By <a href="https://levintoo.me/">levin</a></p>
+        </div>
     </section>
     <!-- footer section end  -->
 
