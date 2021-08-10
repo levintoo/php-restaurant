fed6c589aafba0c1a282eec438afe1924f160e94
levintoo <tooklevin@gmail.com>
2021-08-10 17:20:40 +0300

    admin table

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,14 +1,27 @@
-<?php include('./partials/menu.php')?>
+<?php include('./partials/menu.php') ?>
 
-    <!-- main content section start  -->
-    <section class="main-content">
-        <div class="wrapper row py-lg-4 py-md-3 col-md-12">
-            <p class="fs-2"><strong>Manage Admin</strong></p>
+<!-- main content section start  -->
+<section class="main-content">
+    <div class="wrapper row py-lg-4 py-md-3 col-md-12">
+        <p class="fs-2"><strong>Manage Admin</strong></p>
 
+        <table>
+            <tr>
+            <th>S.N.</th>
+            <th>Full Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Levoo</td>
+                <td>Kevo</td>
+                <td>Delet Admin</td>
+            </tr>
+            
+        </table>
+    </div>
+</section>
+<!-- main content section end  -->
 
-        </div>
-
-    </section>
-    <!-- main content section end  -->
-
-    <?php include('./partials/footer.php')?>
+<?php include('./partials/footer.php') ?>
