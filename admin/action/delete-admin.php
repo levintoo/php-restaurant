c623b05aa56fa21874857f23e7b46fcc039384a6
levintoo <tooklevin@gmail.com>
2021-08-13 16:18:59 +0300

    deleted admmin alert

--- admin/action/delete-admin.php
+++ admin/action/delete-admin.php
@@ -7,20 +7,21 @@ $id = $_GET['id'];
 $query = "DELETE FROM tbl_admin WHERE id =$id";
 $result = mysqli_query($db, $query);
 // check query excution succesful or not
-if($result ==true) {
+if ($result == true) {
     //succesful
     // echo "admin deleted";
     //create session to display message
-    $_SESSION['delete'] = "Admin deleted";
+    $_SESSION['delete'] = '<div class="alert alert-success alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
+    <strong class="me-5">Admin successfully deleted</strong>
+    </div>';
     header('location: http://localhost:7882/wowfood/admin/manage-admin.php');
-}
-else{
+} else {
     //filed
     //echo "filed";
-    $_SESSION['delete'] = "Failed to delete admin";
-    header('location: http://localhost:7882/wowfood/admin/manage-admin.php');
+    $_SESSION['delete'] = '<div class="alert alert-danger alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
+    <strong class="me-5">Failed to delete admin</strong>
 
+</div>';
+    header('location: http://localhost:7882/wowfood/admin/manage-admin.php');
 }
 //redirect to manage admin with message success or failed
-
-?>
