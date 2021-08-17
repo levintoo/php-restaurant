81add50ba8edabf62e5323ee4941d03fe9744c79
levintoo <tooklevin@gmail.com>
2021-08-17 17:11:27 +0300

    categ details

--- admin/update-category.php
+++ admin/update-category.php
@@ -4,7 +4,8 @@ $id = $_GET['id'];
 
 $query = "SELECT * FROM tbl_category WHERE id =$id";
 $result = mysqli_query($db, $query);
-$count = mysqli_num_rows($result);
+if ($result == true){
+    $count = mysqli_num_rows($result);
 
 if ($count ==0){
     $_SESSION['nocateg'] = '<div class="alert text-danger alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
@@ -18,6 +19,9 @@ if ($count ==0){
         $image_name = $data['image_name'];
         $featured = $data['featured'];
         $active = $data['active'];
+}
+}else{
+
 }
 ?>
 
@@ -46,7 +50,7 @@ if ($count ==0){
                 <input type="file" name="image" placeholder="" class="input-responsive" >
 
                 <!-- //featured -->
-                <div class="order-label text-danger">Featured</div>
+                <div class="order-label text-danger">Featured : <span class="text-secondary"><?php echo $featured; ?></span></div>
                 <div class="order-label input-responsive">
                     Yes
                     <input value="Nes" type="radio" name="featured" placeholder="" class="mx-4">
@@ -56,7 +60,7 @@ if ($count ==0){
                 </div>
                 <!-- //end of featured -->
                 <!-- //active -->
-                <div class="order-label text-danger">Active</div>
+                <div class="order-label text-danger">Active : <span class="text-secondary"><?php echo $featured; ?></span></div>
                 <div class="order-label input-responsive">
                     Yes
                     <input value="Yes" type="radio" name="active" placeholder="" class="mx-4">
