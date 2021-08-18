5aed5b500ee2195cf377164fccc94e18f4ba0bf6
levintoo <tooklevin@gmail.com>
2021-08-18 12:35:50 +0300

    categ image disp

--- admin/manage-category.php
+++ admin/manage-category.php
@@ -30,50 +30,52 @@
             <?php
             $query = "SELECT * FROM tbl_category";
             $result = mysqli_query($db, $query);
-            $count = mysqli_num_rows($result);
+            if ($result == true) {
+                $count = mysqli_num_rows($result);
 
-            $sn = 1;
-            if ($count = 0) {
-                $_SESSION['nocateg'] = '<div class="p-0 mb-3 alert text-danger alert-dismissible fade show m-0 w-auto d-flex h-auto align-items-center" role="alert">
-                <strong class=" p-0">No categories found</strong>
-                </div>';
-            } else {
+                $sn = 1;
+                if ($count = 0) {
+                    $_SESSION['nocateg'] = '<div class="p-0 mb-3 alert text-danger alert-dismissible fade show m-0 w-auto d-flex h-auto align-items-center" role="alert">
+        <strong class=" p-0">No categories found</strong>
+        </div>';
+                } else {
 
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $id = $row['id'];
-                    $title = $row['title'];
-                    $image_name = $row['image_name'];
-                    $active = $row['active'];
-                    $featured = $row['featured'];
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $image_name = $row['image_name'];
+                        $active = $row['active'];
+                        $featured = $row['featured'];
 
             ?>
-                    <tr>
-                        <td class="p-2"><?php echo $sn++ ?></td>
-                        <td class="p-2"><?php echo $title; ?></td>
+                        <tr>
+                            <td class="p-2"><?php echo $sn++ ?></td>
+                            <td class="p-2"><?php echo $title; ?></td>
 
-                        <td class="p-2">
-                            <?php
-                            if (!$image_name == "") {
-                            ?>
-                                <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" width="100px" alt="">
-                            <?php
-                            } else {
-                                echo '<span class="text-danger">No image available</span>';
-                            }
-                            ?>
-                        </td>
+                            <td class="p-2">
+                                <?php
+                                if (!$image_name == "") {
+                                ?>
+                                    <img src="<?php echo SITEURL; ?>images/category/<?php echo $image_name; ?>" width="100px" alt="<?php echo $image_name; ?>">
+                                <?php
+                                } else {
+                                    echo '<span class="text-danger">No image available</span>';
+                                }
+                                ?>
+                            </td>
 
-                        <td class="p-2"><?php echo $featured; ?></td>
-                        <td class="p-2"><?php echo $active; ?></td>
-                        <td class="p-2 ">
-                            <span class="d-flex flex-wrap">
-                                <a href="http://localhost:7882/wowfood/admin/update-category.php?id=<?php echo $id?>" class="btn-sm btn-success p-2">Update Category</a>
-                                <a href="http://localhost:7882/wowfood/admin/action/delete-category.php?id=<?php echo $id; ?>" class="me-2 btn-sm btn-danger p-2 " disabled>Delete Category</a>
+                            <td class="p-2"><?php echo $featured; ?></td>
+                            <td class="p-2"><?php echo $active; ?></td>
+                            <td class="p-2 ">
+                                <span class="d-flex flex-wrap">
+                                    <a href="http://localhost:7882/wowfood/admin/update-category.php?id=<?php echo $id ?>" class="btn-sm btn-success p-2">Update Category</a>
+                                    <a href="http://localhost:7882/wowfood/admin/action/delete-category.php?id=<?php echo $id; ?>" class="me-2 btn-sm btn-danger p-2 " disabled>Delete Category</a>
 
-                            </span>
-                        </td>
-                    </tr>
+                                </span>
+                            </td>
+                        </tr>
             <?php
+                    }
                 }
             }
             ?>
--- admin/update-category.php
+++ admin/update-category.php
@@ -1,33 +1,31 @@
-<?php include('./partials/menu.php') ;;
+<?php include('./partials/menu.php');;
 $id = $_GET['id'];
 
 
 $query = "SELECT * FROM tbl_category WHERE id =$id";
 $result = mysqli_query($db, $query);
-if ($result == true){
+if ($result == true) {
     $count = mysqli_num_rows($result);
 
-if ($count ==0){
-    $_SESSION['nocateg'] = '<div class="alert text-danger alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
+    if ($count == 0) {
+        $_SESSION['nocateg'] = '<div class="alert text-danger alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
     <strong class="mx-2">Category Not available</strong>
     </div>';
-    header("Location: http://localhost:7882/wowfood/admin/manage-category.php");
-
-}else{
+        header("Location: http://localhost:7882/wowfood/admin/manage-category.php");
+    } else {
         $data = mysqli_fetch_assoc($result);
         $title = $data['title'];
         $image_name = $data['image_name'];
         $featured = $data['featured'];
         $active = $data['active'];
-}
-}else{
-
+    }
+} else {
 }
 ?>
 
 <style>
     .food-search {
-        height: 100vh;
+        height: 110vh;
     }
 </style>
 <!-- fOOD sEARCH Section Starts Here -->
@@ -41,13 +39,12 @@ if ($count ==0){
                 <legend></legend>
 
                 <div class="order-label">Title</div>
-                <input type="text" name="title" placeholder="" value="<?php echo $title; ?>" class="input-responsive" >
-
-                <div class="order-label">Image Name</div>
-                <input type="text" name="title" placeholder="" value="<?php echo $image_name; ?>" class="input-responsive" disabled>
+                <input type="text" name="title" placeholder="" value="<?php echo $title; ?>" class="input-responsive">
 
                 <div class="order-label">Image</div>
-                <input type="file" name="image" placeholder="" class="input-responsive" >
+                <img src="../images/category/<?php echo $image_name; ?>" alt="Unavailable" height="100px">
+                <div class="order-label">Image</div>
+                <input type="file" name="image" placeholder="" class="input-responsive">
 
                 <!-- //featured -->
                 <div class="order-label text-danger">Featured : <span class="text-secondary"><?php echo $featured; ?></span></div>
@@ -80,5 +77,3 @@ if ($count ==0){
 
 
 <?php include('./partials/footer.php'); ?>
-
-
