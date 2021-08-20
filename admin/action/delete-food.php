daa14a2e3cb244e0fbfefe60fb697a8595d6ac0b
levintoo <tooklevin@gmail.com>
2021-08-20 20:59:12 +0300

    deleting image and food from db

--- admin/action/delete-food.php
+++ admin/action/delete-food.php
@@ -12,14 +12,24 @@ if (isset($_GET['id']) && isset($_GET['image_name'])) {
             if ($image_name ==""){
                 $query = "DELETE FROM tbl_food WHERE id =$id";
                 $result = mysqli_query($db, $query);
-
-                $_SESSION['deletefood'] = '<div class="alert alert-success alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
-                <strong class="mx-2">Food successfully deleted</strong>
-                </div>';
-                header('location: http://localhost:7882/wowfood/admin/manage-food.php');
+                if ($result == true) {
+                    $_SESSION['deletefood'] = '<div class="alert alert-success alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
+                    <strong class="mx-2">Food successfully deleted</strong>
+                    </div>';
+                    header('location: http://localhost:7882/wowfood/admin/manage-food.php');
+                }
             }else{
                 $path = "../../images/food/$image_name";
-                echo '<img src="$path">';
+                $remove = unlink($path);
+                $query = "DELETE FROM tbl_food WHERE id =$id";
+                $result = mysqli_query($db, $query);
+
+                if ($result == true) {
+                    $_SESSION['deletefood'] = '<div class="alert alert-success alert-dismissible fade show p-2 w-auto d-flex h-auto align-items-center" role="alert">
+                    <strong class="mx-2">Food successfully deleted</strong>
+                    </div>';
+                    header('location: http://localhost:7882/wowfood/admin/manage-food.php');
+                }
             }
         }
 }
