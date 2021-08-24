8e79794f090dd46b023964bbb77327fd854d5f48
levintoo <tooklevin@gmail.com>
2021-08-24 16:42:46 +0300

    displaying food orders in admin

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -6,51 +6,67 @@
 
             <div class="mb-3">
         </div>
-        <table>
-            <tr class="border-bottom">
-            <th class="p-2">S.N.</th>
-            <th class="p-2">Full Name</th>
-            <th class="p-2">Username</th>
+        <table class="">
+            <tr class="border-bottom ">
+            <th class="p-2 ">S.N.</th>
+            <th class="p-2">Food</th>
+            <th class="p-2">Price</th>
+            <th class="p-2">Qty.</th>
+            <th class="p-2">Total</th>
+            <th class="p-2">Order Date</th>
+            <th class="p-2">Order Type</th>
+            <th class="p-2">delivery date</th>
+            <th class="p-2">Status</th>
+            <th class="p-2">Customer Name</th>
+            <th class="p-2">Contact</th>
+            <th class="p-2">Address</th>
+            <th class="p-2">Email</th>
             <th class="p-2">Actions</th>
             </tr>
+            <?php 
+                $query = "SELECT * FROM tbl_order";
+                $result = mysqli_query($db,$query);
+                $count = mysqli_num_rows($result);
+                while ($row= mysqli_fetch_assoc($result)){
+                    $food= $row['food'];
+                    $price = $row['price'];
+                    $qty = $row['quantity'];
+                    $total = $row['total'];
+                    $order_date = $row['order_date'];
+                    $order_type = $row['order_type'];
+                    $delivery_date = $row['delivery_date'];
+                    $status = $row['delivery_status'];
+                    $customer_name = $row['customer_name'];
+                    $contact = $row['customer_contact'];
+                    $address = $row['customer_address'];
+                    $email = $row['customer_email'];
+
+                    ?>
             <tr>
                 <td class="p-2">1</td>
-                <td class="p-2">Levoo</td>
-                <td class="p-2">Levo</td>
+                <td class="p-2"><?php echo $food;?></td>
+                <td class="p-2"><?php echo $price;?></td>
+                <td class="p-2"><?php echo $qty;?></td>
+                <td class="p-2"><?php echo $total;?></td>
+                <td class="p-2"><?php echo $order_date;?></td>
+                <td class="p-2"><?php echo $order_type;?></td>
+                <td class="p-2"><?php echo $delivery_date;?></td>
+                <td class="p-2"><?php echo $status;?></td>
+                <td class="p-2"><?php echo $customer_name;?></td>
+                <td class="p-2"><?php echo $contact;?></td>
+                <td class="p-2"><?php echo $address;?></td>
+                <td class="p-2"><?php echo $email;?></td>
                 <td class="p-2 flex-wrap d-flex">
-                    <a href="" class="btn-sm btn-success p-2">Update Admin</a>
-                    <a href="" class="btn-sm btn-danger p-2">Delete Admin</a>
+                    <a href="" class="btn-sm btn-success p-2">Update Order</a>
+                    <a href="" class="btn-sm btn-danger p-2">Delete Delete</a>
                 </td>
             </tr>
-            <tr>
-                <td class="p-2">2</td>
-                <td class="p-2">Levoo</td>
-                <td class="p-2">Levo</td>
-                <td class="p-2">
-                    <a href="" class="btn-sm btn-success p-2">Update Admin</a>
-                    <a href="" class="btn-sm btn-danger p-2">Delete Admin</a>
-                   
-                </td>
-            </tr> <tr>
-                <td class="p-2">3</td>
-                <td class="p-2">Levoo</td>
-                <td class="p-2">Levo</td>
-                <td class="p-2">
-                    <a href="" class="btn-sm btn-success p-2">Update Admin</a>
-                    <a href="" class="btn-sm btn-danger p-2">Delete Admin</a>
-                   
-                </td>
-            </tr> <tr>
-                <td class="p-2">4</td>
-                <td class="p-2">Levoo</td>
-                <td class="p-2">Levo</td>
-                <td class="p-2">
-                    <a href="" class="btn-sm btn-success p-2">Update Admin</a>
-                    <a href="" class="btn-sm btn-danger p-2">Delete Admin</a>
-                   
-                </td>
-            </tr>
+                    <?php
+                }
             
+            ?>
+
+
         </table>
         </div>
 </div>
